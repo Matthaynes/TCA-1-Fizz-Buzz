a6076159a23b349047f1d3cc1f616899d143b571
Matthaynes <90624248+Matthaynes@users.noreply.github.com>
2023-11-02 08:14:59 -0400

    "solved"

--- script.js
+++ script.js
@@ -1,24 +1,26 @@
 function fizzbuzz(n) {
 	n=[]
 while(n<16) {
-	if (Number.isInteger(n/3) && n>0){
-	if (Number.isInteger(n/5) && n>0){
+	if (Number.isInteger(n>10){
+	if (Number.isInteger(n/3) {
+	if (Number.isInteger(n/5){
 		console.log("FizzBuzz"); n++
 	}
 	else {
 		console.log("Fizz"); n++
 	}
 }
-if (Number.isInteger(n/5) && n>0){
-	if(Number.isInteger(n/3) && n>0){
+if (Number.isInteger(n/5){
+	if(Number.isInteger(n/3){
 		console.log("FizzBuzz"); n++
 	}
 	else {
 		console.log("Buzz"); n++
 	}
 }
-else if (Number.isInteger(n) && n>0){
+else if (Number.isInteger(n) {
 	console.log(n); n++
+}
 }
 }
 	// Leave the console.log below at the bottom of the function
